981e7b1780400b8d6c4f5ecce825f183be1256c3
tsunami-code <61623464+tsunami-code@users.noreply.github.com>
2020-04-07 00:58:50 +0800

    Defining a Provisioner for remote-exec

--- example.tf
+++ example.tf
@@ -1,13 +1,30 @@
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/terraform.pub")
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-04590e7389a6e577c"
   instance_type = "t2.micro"
 
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/terraform")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
   }
 }
