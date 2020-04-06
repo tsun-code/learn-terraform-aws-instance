c90e9012d80b942895dc31e6da29aaf098de86ae
tsunami-code <61623464+tsunami-code@users.noreply.github.com>
2020-04-07 00:53:35 +0800

    Defining a Provisioner

--- example.tf
+++ example.tf
@@ -3,31 +3,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-# New resource for the S3 bucket our application will use.
-resource "aws_s3_bucket" "example" {
-  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
-  # this name must be changed before applying this example to avoid naming
-  # conflicts.
-  bucket = "terraform-getting-started-guide"
-  acl    = "private"
-}
-
-# Change the aws_instance we declared earlier to now include "depends_on"
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-
-  # Tells Terraform that this EC2 instance must be created only after the
-  # S3 bucket has been created.
-  depends_on = [aws_s3_bucket.example]
-}
-
-resource "aws_instance" "another" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
-}
 
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
